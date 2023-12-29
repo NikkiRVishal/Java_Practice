a4cfc7bc907d02d393e5103a4b4f665a6555d1da
Nikita <nikisharma105@gmail.com>
2023-12-29 05:39:55 -0800

    Commiting the project from IDEA/Intellij

---
+++ src/Java_Test.java
@@ -0,0 +1,15 @@
+public class Java_Test {
+   private String name;
+
+   public String getName()//getter
+   {
+       return name;
+   }
+
+   public void setName(String name)
+   {
+       this.name = name;
+   }
+
+
+}
---
+++ src/Test2.java
@@ -0,0 +1,7 @@
+public class Test2 {
+    public static void main(String[] args) {
+        Java_Test myobj = new Java_Test();
+        myobj.setName("Hritik");
+        System.out.println(myobj.getName());
+    }
+}
